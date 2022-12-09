e76bd90de95bb3b008179cbbccb1af9bb6d2e72f
translateindobot <118644068+translateindobot@users.noreply.github.com>
2022-12-09 18:31:43 +0700

    Create 14. ScriptDB.js

---
+++ 14. ScriptDB.js
@@ -0,0 +1,45 @@
+/**
+* Kelas untuk Script Properti
+* @type {Class} klass utama
+*
+* Adopsi dari Lib2
+*/
+var scriptDB = class scriptProperties {
+  /**
+  *initialize constructor
+  */
+  constructor() {
+    this.service = PropertiesService.getScriptProperties();
+  }
+
+  setValue(kunci, nilai) {
+    // contoh: setValue('token', '123:xxxx');
+    return this.service.setProperty(kunci, nilai);
+  }
+
+  setValues(data) {
+    // contoh {nickname: 'Bob', region: 'US', language: 'EN'};
+    return this.service.setProperties(data);
+  }
+
+  getValue(kunci) {
+    // contoh: getValue('token');
+    return this.service.getProperty(kunci);
+  }
+
+  getValues() {
+    return this.service.getProperties();
+  }
+
+  getKeys() {
+    return this.service.getKeys();
+  }
+
+  delete(kunci) {
+    return this.service.deleteProperty(kunci);
+  }
+
+  deleteAll() {
+    return this.service.deleteAllProperties();
+  }
+}
